ea70497c8b681b7123f28b2a67530371cccd5f8b
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-07-04 15:44:26 +0530

    remove extra usings

--- DivideByK.cs
+++ DivideByK.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PairDivisibleByK
+﻿namespace PairDivisibleByK
 {
     public class DivideByK
     {
